f7f21f613cfc26c70788dbf99771b22998b5e7c9
Szabolcs Gábor Kiss <2021132@dszcberegszaszi.hu>
2024-09-11 08:57:46 +0200

    tszt

---
+++ Tszt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ide majd írok valamit
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
