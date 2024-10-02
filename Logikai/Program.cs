adcbd45fd0625c8b36b4a7cce63d0cc809830d8c
KissSzabolcs2006 <2021132@dszcberegszaszi.hu>
2024-10-02 08:52:34 +0200

    Logic

---
+++ Logikai/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logikai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true,
+                b = false;
+            Console.WriteLine("Az és (&&) kapcsolat");
+            Console.WriteLine($"Ha a={a} és b={b} akkor a&&b = {a && b}");
+
+            a = true;
+            b = true;
+
+            Console.WriteLine($"Ha a={a} és b={b} akkor a&&b = {a && b}");
+            a = false;
+            b = true;
+
+            Console.WriteLine($"Ha a={a} és b={b} akkor a&&b = {a && b}");
+            a = false;
+            b = false;
+
+            Console.WriteLine($"Ha a={a} és b={b} akkor a&&b = {a && b}");
+            Console.ReadLine();
+
+            //---------------------------------------------------------------------------
+
+            a = true;
+            b = false;
+            Console.WriteLine("\n Az vagy (||) kapcsolat");
+            Console.WriteLine($"Ha a={a} és b={b} akkor a||b = {a || b}");
+
+            a = true;
+            b = true;
+
+            Console.WriteLine($"Ha a={a} és b={b} akkor a||b = {a || b}");
+            a = false;
+            b = true;
+
+            Console.WriteLine($"Ha a={a} és b={b} akkor a||b = {a || b}");
+            a = false;
+            b = false;
+
+            Console.WriteLine($"Ha a={a} és b={b} akkor a||b = {a || b}");
+            Console.ReadLine();
+        }
+    }
+}
