f4d48719da4977605ed42ef42f83c2f5c8ee755b
KissSzabolcs2006 <2021132@dszcberegszaszi.hu>
2024-10-02 09:53:20 +0200

    Haág prograM

---
+++ Ha/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+            if (b) Console.WriteLine("Ez az igaz");
+            else Console.WriteLine("Ez a hamis");
+
+            b = false;
+            if (b) Console.WriteLine("Ez az igaz");
+            else Console.WriteLine("Ez a hamis");
+
+            Console.ReadLine();
+        }
+    }
+}
