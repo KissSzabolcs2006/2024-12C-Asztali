db524ea5dd64407b5396fa0bf886259148fbc950
KissSzabolcs2006 <2021132@dszcberegszaszi.hu>
2024-10-09 09:13:40 +0200

    Kor

---
+++ KorHasab/Kor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KorHasab
+{
+    class Kor
+    {
+        //Az osztáy feladata: A sugárból kiszámítja a kör területét, kerületét.
+
+        //Osztályváltozók
+        private double sugar, kerulet, terulet;
+
+        //Konstruktorok
+        public Kor()
+        {
+
+        }
+
+        public Kor(double r)
+        {
+            this.sugar = r;
+        }
+
+        //Metódusok
+        public void Readsugar(double r)
+        {
+            this.sugar = r;
+        }
+
+        public void SetKerület()
+        {
+            
+            this.kerulet = 2 * this.sugar * Math.PI;
+        }
+        public void SetTerület()
+        {
+            double eredmeny1 = 0.0;
+            double eredmeny2 = 0.0;
+            eredmeny2 = this.terulet= this.sugar*this.sugar*Math.PI;
+            eredmeny1 = Math.Pow(this.sugar, 2)*Math.PI;
+            if(eredmeny2 == eredmeny1)
+            {
+                this.terulet = eredmeny1;
+            }
+            else
+            {
+                Console.WriteLine($"Eredmény1 ({eredmeny1}) != Eredmeny2 ({eredmeny2})");
+            }
+        }
+        public double GetKerulet()
+        {
+            return this.kerulet;
+        }
+        public double GetTerulet()
+        {
+            return this.terulet;
+        }
+        public double GetSugar()
+        {
+            return this.sugar;
+        }
+    }
+}
---
+++ KorHasab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KorHasab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A kor osztály felhasználása
+            #region 1-es Objektum
+            //1. objektum
+            Kor k1 = new Kor();
+            k1.Readsugar(15.0);
+            k1.SetKerület();
+            k1.SetTerület();
+            Console.WriteLine($"A {k1.GetSugar()} sugarú kör kerülete {k1.GetKerulet()} területe {k1.GetTerulet()}");
+            Console.ReadLine();
+            #endregion
+            #region 2-es objektum
+            Kor k2 = new Kor(28.7);
+            k2.SetKerület();
+            k2.SetTerület();
+            Console.WriteLine($"A {k2.GetSugar()} sugarú kör kerülete {k2.GetKerulet()} területe {k2.GetTerulet()}");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
