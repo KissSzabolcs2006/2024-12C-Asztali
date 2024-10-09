dc04edd60d176a0191127eb842c2f4e6b819283d
KissSzabolcs2006 <2021132@dszcberegszaszi.hu>
2024-10-09 10:10:12 +0200

    tegla

---
+++ teglalap/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KorHasab
+{
+    class Tegla
+    {
+        private double a, b, kerulet, terulet;
+
+        public Tegla()
+        {
+
+        }
+
+        public void SetKerület()
+        {
+
+            this.kerulet = 2 * (a + b);
+        }
+        public void SetTerület()
+        {
+            double eredmeny1 = 0.0;
+            double eredmeny2 = 0.0;
+            eredmeny2 = this.terulet = a * b;
+            if (eredmeny2 != 0)
+            {
+                this.terulet = eredmeny1;
+            }
+        }
+        public double GetKerulet()
+        {
+            return this.kerulet;
+        }
+        public double GetTerulet()
+        {
+            return this.terulet;
+        }
+    }
+}
---
+++ teglalap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KorHasab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A kor osztály felhasználása
+            #region 1-es Objektum
+            //1. objektum
+            Tegla k1 = new Tegla();
+            k1.SetKerület();
+            k1.SetTerület();
+            Console.WriteLine($"A teglalap kerülete {k1.GetKerulet()} területe {k1.GetTerulet()}");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
