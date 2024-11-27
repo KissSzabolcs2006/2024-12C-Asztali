e4246fc187653fc4cea093e1f558663414714246
KissSzabolcs2006 <2021132@dszcberegszaszi.hu>
2024-11-27 10:05:19 +0100

    Igen2

---
+++ Kivetel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kivetelClass k = new kivetelClass();
+            /*
+            k.kivkeznincs();
+            
+            k.KivKezAlt();
+            */
+            k.KivkezOS();
+        }
+    }
+}
---
+++ Kivetel/kivetelClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetel
+{
+    class kivetelClass
+    {
+        public kivetelClass() { }
+        public void kivkeznincs()
+        {
+            Console.WriteLine("Nincs kivételekezelés!");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A beírt szám: {0}", szam);
+            Console.ReadKey();
+        }
+
+        public void KivKezAlt()
+        {
+            Console.WriteLine("Hiba!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A beírt szám: {0}", szam);
+            }
+            catch
+            {
+                throw;
+            }
+            Console.ReadKey();
+        }
+
+        public void KivkezOS()
+        {
+
+            Console.WriteLine("Hiba!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A beírt szám: {0}", szam);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
